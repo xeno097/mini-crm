ccc473318a4be5276577ef1455c1d78f27084251
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-06-14 19:24:34 -0400

    added mongoose module configuration to main app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
+import { EnvKey } from './config/enum/env-key.enum';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       cache: true,
       isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService): MongooseModuleOptions => {
+        const db_uri = configService.get<string>(EnvKey.DB_URI);
+
+        return {
+          uri: db_uri,
+          useCreateIndex: true,
+          useFindAndModify: false,
+        };
+      },
     }),
   ],
   controllers: [],
