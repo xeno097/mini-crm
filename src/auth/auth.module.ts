bd12c430ea2f5b30da4ca4820c38a813b59d6629
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-06-14 23:37:08 -0400

    imported user module in auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { AuthResolver } from './auth.resolver';
 import { AuthRepository } from './auth.repository';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthEntity, AuthEntitySchema } from './database/auth.entity';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { AuthEntity, AuthEntitySchema } from './database/auth.entity';
         schema: AuthEntitySchema,
       },
     ]),
+    UserModule,
   ],
   providers: [AuthResolver, AuthService, AuthRepository],
 })
