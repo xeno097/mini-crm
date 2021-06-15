f396fcef02af410adbedcb60fa398e2722143e70
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-06-14 23:03:42 -0400

    added auth entity to mongoose module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,8 +2,18 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
 import { AuthRepository } from './auth.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthEntity, AuthEntitySchema } from './database/auth.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: AuthEntity.name,
+        schema: AuthEntitySchema,
+      },
+    ]),
+  ],
   providers: [AuthResolver, AuthService, AuthRepository],
 })
 export class AuthModule {}
