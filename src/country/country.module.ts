51722fdd9e64bbdcc75e35c12a7a7c572f3b4e4f
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-06-15 11:57:03 -0400

    added mongoose module configuration to country module

--- src/country/country.module.ts
+++ src/country/country.module.ts
@@ -2,8 +2,19 @@ import { Module } from '@nestjs/common';
 import { CountryService } from './country.service';
 import { CountryResolver } from './country.resolver';
 import { CountryRepository } from './country.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CountryEntity, CountryEntitySchema } from './database/country.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: CountryEntity.name,
+        schema: CountryEntitySchema,
+      },
+    ]),
+  ],
   providers: [CountryResolver, CountryService, CountryRepository],
+  exports: [MongooseModule],
 })
 export class CountryModule {}
